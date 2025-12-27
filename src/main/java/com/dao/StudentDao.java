2b8e1e8655c7c62187235773d3bd24d5f6ad6b23
Haridas250503 <haridasshinde314@gmail.com>
2025-12-28 01:06:11 +0530

    adding the configuration

--- src/main/java/com/dao/StudentDao.java
+++ src/main/java/com/dao/StudentDao.java
@@ -1,9 +1,19 @@
 package com.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.model.Student;
 
 public class StudentDao {
 	
+	private JdbcTemplate j;
+	
+
+	public void setJ(JdbcTemplate j) {
+		this.j = j;
+	}
+
+
 	public int insertStudent(Student s) {
 		return 0;
 	}
